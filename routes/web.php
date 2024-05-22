db03362b517b370ec0c75b9a7b169d381c211b2b
amely11 <teresiaamelia12@gmail.com>
2024-05-22 20:38:34 +0700

    First Commit
    First Commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PermissionController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+/*
+|-------------------------------------------------------------------------
+-
+| Web Routes
+|-------------------------------------------------------------------------
+-
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class,
+        'index'])->name('home');
+    Route::resources([
+        'roles' => RoleController::class,
+        'users' => UserController::class,
+        'mahasiswas' => MahasiswaController::class,
+        'permissions' => PermissionController::class,
+    ]);
+});
